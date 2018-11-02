7f29da71b9913f916ab46e88fe46c52fe67ad916
irshad-ahmed <irshad.ahmed000@gmail.com>
2018-11-02 09:17:40 +0530

    More changes

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Mr. Nice'},
+    { id: 12, name: 'Narco'},
+    { id: 13, name: 'Superman'},
+    { id: 14, name: 'Shaktimaan'},
+    { id: 15, name: 'Magneta'},
+    { id: 16, name: 'rubberMan'},
+    { id: 17, name: 'Batman'},
+    { id: 18, name: 'Dynama'},
+    { id: 19, name: 'Spiderman'},
+    { id: 20, name: 'Antman'}
+]
